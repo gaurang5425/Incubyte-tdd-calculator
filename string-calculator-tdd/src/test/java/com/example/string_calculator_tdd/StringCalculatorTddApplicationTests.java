848c389210df1d055457311d9f685eb584054db6
gaurang5425 <163129474+gaurang5425@users.noreply.github.com>
2025-07-04 21:56:09 +0530

    Add failing test for single number

--- string-calculator-tdd/src/test/java/com/example/string_calculator_tdd/StringCalculatorTddApplicationTests.java
+++ string-calculator-tdd/src/test/java/com/example/string_calculator_tdd/StringCalculatorTddApplicationTests.java
@@ -14,4 +14,10 @@ class StringCalculatorTddApplicationTests {
 		assertEquals(0, calculator.add(""));
 	}
 
+	@Test
+	public void should_return_number_for_single_number() {
+		StringCalculator calculator = new StringCalculator();
+		assertEquals(1, calculator.add("1"));
+		assertEquals(5, calculator.add("5"));
+	}
 }
