056dc1d6a68c9ef92d3eac1fa9e6a2bd121f4c59
gaurang5425 <163129474+gaurang5425@users.noreply.github.com>
2025-07-04 21:59:17 +0530

    Make single number test pass

--- string-calculator-tdd/src/main/java/com/example/string_calculator_tdd/StringCalculator.java
+++ string-calculator-tdd/src/main/java/com/example/string_calculator_tdd/StringCalculator.java
@@ -2,6 +2,9 @@ package com.example.string_calculator_tdd;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;  // Minimal code to pass test
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(numbers);
     }
 }
